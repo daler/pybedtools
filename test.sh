59c28b918d5f7afeaaedadbb0cf93dcc678974e9
daler <dalerr@niddk.nih.gov>
2011-04-25 14:53:25 -0400

    commented-out line in test.sh for running sphinx doctests

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 python setup.py build_ext -i && \
-PATH=$PATH:../bedtools/bin/ nosetests --with-doctest --doctest-extension=.pyx pybedtools/cbedtools.pyx .
+PATH=$PATH:../bedtools/bin/ nosetests --with-doctest --doctest-extension=.pyx pybedtools/cbedtools.pyx .&& \
+# cd docs && make doctest  && make html
