a150c63e6aa67da7be66c58c5eb2b776732c9666
Brent Pedersen (brentp) <bpederse@gmail.com>
2011-05-17 14:27:31 -0600

    cleanup

--- pybedtools/test/test_scripts.py
+++ pybedtools/test/test_scripts.py
@@ -1,16 +1,17 @@
 import pybedtools
+from pybedtools.scripts import annotate
 from nose.tools import assert_raises
 
 
 
 def test_annotate_main():
     # exits after printing help when sys.argv is not as it should be.
-    assert_raises(SystemExit, pybedtools.scripts.annotate.main)
+    assert_raises(SystemExit, annotate.main)
 
 def test_annotate_closest():
     a = pybedtools.example_bedtool('a.bed')
     b = pybedtools.example_bedtool('b.bed')
-    c = pybedtools.scripts.annotate.add_closest(a, b)
+    c = annotate.add_closest(a, b)
     assert len(a) == len(c), (len(a), len(c), str(c))
     assert a.field_count() == c.field_count() - 2
 
@@ -18,8 +19,8 @@ def test_annotate_closest():
 def test_annotate_xstream():
     a = pybedtools.example_bedtool('a.bed')
     b = pybedtools.example_bedtool('b.bed')
-    c = pybedtools.scripts.annotate.add_xstream(a, b, dist=1000, updown="up")
+    c = annotate.add_xstream(a, b, dist=1000, updown="up")
     assert a.field_count() == c.field_count() - 1
     assert len(a) == len(c)
-    d = pybedtools.scripts.annotate.add_xstream(c, b, dist=1000, updown="down")
+    d = annotate.add_xstream(c, b, dist=1000, updown="down")
     assert a.field_count() == d.field_count() - 2
