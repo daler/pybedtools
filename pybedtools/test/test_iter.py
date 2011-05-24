ae3c124586d0e8a752a65dfe93590c41471c1374
ryan <sendjunktoryan@gmail.com>
2011-05-23 23:24:48 -0400

    new YAML-based test generator

--- pybedtools/test/test_iter.py
+++ pybedtools/test/test_iter.py
@@ -1,10 +1,14 @@
-
+import itertools
+import yaml
+import os
 import pybedtools
 # The functools.partial trick to get descriptions to be valid is from:
 #
 #   http://code.google.com/p/python-nose/issues/detail?id=244#c1
 from functools import partial
 
+this_dir = os.path.dirname(__file__)
+config_fn = os.path.join(this_dir, 'test_cases.yaml')
 
 
 def fix(x):
@@ -24,51 +28,49 @@ def fix(x):
     return s
 
 
-# Actual test looks like this.
+def parse_yaml(infile):
+    x = yaml.load(open(infile).read())
+    for test_case in x:
+        method = test_case['method']
+        send_kwargs = test_case['kwargs']
+        expected = test_case['expected']
+        yield method, send_kwargs, expected
+
+
 def run(method, a, b, expected, **kwargs):
     result = getattr(a, method)(b, **kwargs)
+    print 'Got:'
     print result
+    print 'Expected:'
+    print expected
     assert str(result) == expected
 
-# Generator that yields tests, inserting `a` and `b` as needed
-def test():
+def test_a_b_methods():
+    """
+    Generator that yields tests, inserting different versions of `a` and `b` as
+    needed
+    """
+    for method, send_kwargs, expected in parse_yaml(config_fn):
 
-    a = pybedtools.example_bedtool('a.bed')
-    orig_b = pybedtools.example_bedtool('b.bed')
+        if not ('a' in send_kwargs) and ('b' in send_kwargs):
+            continue
 
-    # Contains (kwargs, expected) tuples.
-    # TODO: refactor this into YAML
-    expecteds = (
-                   (dict(s=True), fix("""
-                             chr1	155	200	feature3	0	-
-                             chr1	900	901	feature4	0	+
-                             """)),
+        orig_a = pybedtools.example_bedtool(send_kwargs['a'])
+        orig_b = pybedtools.example_bedtool(send_kwargs['b'])
 
-                   (dict(s=False), fix("""
-                             chr1	155	200	feature2	0	+
-                             chr1	155	200	feature3	0	-
-                             chr1	900	901	feature4	0	+
-                             """)),
-                )
+        del send_kwargs['a']
+        del send_kwargs['b']
 
-    for send_kwargs, expected in expecteds:
-        b = orig_b
-        kind = 'file'
-        method = 'intersect'
-        f = partial(run, method, a, b, expected, **send_kwargs)
-        f.description = '%(method)s, %(kind)s, %(send_kwargs)s' % locals()
-        yield (f, )
+        converter = {'file': lambda x: pybedtools.BedTool(x.fn),
+                     'generator': lambda x: pybedtools.BedTool(i for i in x),
+                     'stream': lambda x: pybedtools.BedTool(open(x.fn))
+                    }
+        done = []
+        for kind_a, kind_b in itertools.permutations(('file', 'generator', 'stream'), 2):
+                a = converter[kind_a](orig_a)
+                b = converter[kind_b](orig_b)
+                kind = 'a=%(kind_a)s, b=%(kind_b)s' % locals()
+                f = partial(run, method, a, b, expected, **send_kwargs)
+                f.description = '%(method)s, %(kind)s, %(send_kwargs)s' % locals()
+                yield (f, )
 
-        b = (i for i in orig_b)
-        kind = 'generator'
-        method = 'intersect'
-        f = partial(run, method, a, b, expected, **send_kwargs)
-        f.description = '%(method)s, %(kind)s, %(send_kwargs)s' % locals()
-        yield (f, )
-
-        b = open(orig_b.fn)
-        kind = 'stream'
-        method = 'intersect'
-        f = partial(run, method, a, b, expected, **send_kwargs)
-        f.description = '%(method)s, %(kind)s, %(send_kwargs)s' % locals()
-        yield (f, )
