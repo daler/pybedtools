9f26cb7486c924bd7255aa6ae6967cbb604ead30
Brent Pedersen <bpederse@gmail.com>
2011-06-27 16:07:43 -0600

    revert exons

--- pybedtools/scripts/annotate.py
+++ pybedtools/scripts/annotate.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """
     %prog [options]
 
@@ -15,24 +14,25 @@ for each of the up and downstream columns.
 import argparse
 import sys
 from pybedtools import BedTool
-from pybedtools.cbedtools import parse_attributes, create_interval_from_list
+from pybedtools.cbedtools import parse_attributes
 import collections
 
+# PYTHONPATH=$PYTHONPATH:. python scripts/annotate.py -a data/new.regions.bed -b data/Homo_sapiens.hg18.gtf --upstream 5000
+
+# $ pybedtools annotate -a regions.bed -b knownGene.bed --upstream 10000
+#                  --downstream 5000 --report-distance
+# a bed: regions.bed and another:
+# annotation.bed, it would add 4 columns to regions.bed:
+# nearest-feature, nearest-distance, upstream-features, downstream-features
+# where the up/downstream features are determined by a distance
+# parameter, e.g. like --upstream 10000 --downstream 5000
 
 def get_gff_name(field):
-    """
-    Returns the name, parsed from the GFF attributes field, *field*
-    """
     attrs = parse_attributes(field)
     for key in ("ID", "gene_name", "transcript_id", "gene_id", "Parent"):
-        if key in attrs:
-            return attrs[key]
-
+        if key in attrs: return attrs[key]
 
 def gen_get_name(b, afields):
-    """
-    Factory for making name-getter functions in a format-specific way
-    """
     btype = b.file_type
     if btype == "bed":
         get_name = lambda fields: fields[afields + 3]
@@ -41,105 +41,41 @@ def gen_get_name(b, afields):
             return get_gff_name(fields[afields + 7])
     else:
         raise Exception("not implemented")
-    return get_name
+    return get_name 
 
-
-def add_closest(aname, bname, save_intermediates=False):
-    """
-    Tags each feature in *aname* with the closest feature, or if the closest
-    feature had a distance of 0, figure out if it was in an intron or exon.
-    """
-    a, full_b = BedTool(aname), BedTool(bname)
-
-    def exonify(f):
-        f[4] = "exon"
-        return f
-
-    # Construct introns from a BED12
-    bintrons = full_b.introns()
-
-    # From that same BED12, split out the exons and put 'exon' in the score
-    # field
-    bexons = full_b.bed6().each(exonify)
-
-    fb = open(BedTool._tmp(), "w")
-    for bi in bintrons:
-        fb.write(str(bi) + "\n")
-    for be in bexons:
-        fb.write(str(be) + "\n")
-    fb.close()
-
-    if save_intermediates:
-        bfn = 'b.bed'
-    else:
-        bfn = None
-    b = BedTool(fb.name).sort().saveas(bfn)
-
-    # Alternatively:
-    # b = bintrons.cat(bexons).sort().saveas()
+def add_closest(aname, bname):
+    a, b = BedTool(aname), BedTool(bname)
 
     afields = a.field_count()
-
-    # d=True will report distance to closest feature
-    # t='all' will report all ties instead of choosing just one to report.
-    if save_intermediates:
-        cfn = 'c.bed'
-    else:
-        cfn = None
-    c = a.closest(b, d=True, t="all").saveas(cfn)
-
-    # Create a name-getter function based on what kind of file *a* and *b* are
+    c = a.closest(b, d=True)
     get_name = gen_get_name(b, afields)
 
     dbed = open(BedTool._tmp(), "w")
-
-    # keep the name, distance and feature type.
+    # keep the name and distance
     seen_by_line = collections.defaultdict(list)
-    assert len(c) > 1
     for feat in c:
-        fields = feat.fields
+        key = "\t".join(feat[:afields])
+        seen_by_line[key].append([feat[-1], get_name(feat)])
 
-        # Key into the dict is the reconstituted *a* feature
-        key = "\t".join(fields[:afields])
-        distance = int(fields[-1])
-        seen_by_line[key].append({'distance': distance, 'name': get_name(feat),
-                                  'type': fields[afields + 4]})
-
-    for key, dist_info in seen_by_line.iteritems():
-        if len(dist_info) > 1:
-            assert len(set(d['distance'] for d in dist_info)) == 1, (dist_info)
-
-        # if the distance is zero. figure it it's intron or exon.
-        dist = dist_info[0]['distance']
-        if all(d['distance'] == 0 for d in dist_info):
-            dist = ";".join(sorted(set(d['type'] for d in dist_info)))
-
-        names = ",".join(sorted(set(d['name'] for d in dist_info)))
-
-        new_line = "\t".join([key, names, str(dist)])
+    for key, dist_names in seen_by_line.iteritems():
+        if len(dist_names) > 0:
+            assert len(set([d[0] for d in dist_names])) == 1
+        names = ",".join(sorted(set(d[1] for d in dist_names)))
+        new_line = "\t".join([key] + [names] + [dist_names[0][0]])
         dbed.write(new_line + "\n")
     dbed.close()
-
-    if save_intermediates:
-        dfn = 'd.bed'
-    else:
-        dfn = None
-    d = BedTool(dbed.name).saveas(dfn)
-
+    d = BedTool(dbed.name)
     assert len(d) == len(a)
     return d
-
 
 def add_xstream(a, b, dist, updown, report_distance=False):
     # run a window up or downstream.
     dir = dict(up="l", down="r")[updown]
-    kwargs = {'sw': True, dir: dist}
+    kwargs = {'sw':True, dir: dist}
 
     # have to set the other to 0
-    if "l" in kwargs:
-        kwargs["r"] = 0
-    else:
-        kwargs["l"] = 0
+    if "l" in kwargs: kwargs["r"] = 0
+    else: kwargs["l"] = 0
 
     c = a.window(b, **kwargs)
     afields = a.field_count()
@@ -159,15 +95,13 @@ def add_xstream(a, b, dist, updown, report_distance=False):
     # write the entries that did not appear in the window'ed Bed
     for row in a:
         key = "\t".join(row[:afields])
-        if key in seen:
-            continue
+        if key in seen: continue
         d.write(str(row) + "\t.\n")
 
     d.close()
     dbed = BedTool(d.name)
     assert len(dbed) == len(a)
     return dbed
-
 
 def main():
     """
@@ -198,8 +132,6 @@ def main():
 
     for row in c.sort():
         print(row)
-
-    pybedtools.cleanup()
 
 if __name__ == "__main__":
     import doctest
