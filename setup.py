4d4a4b6227033ce80faa4df99d574d2e7dbd3201
daler <dalerr@niddk.nih.gov>
2011-07-08 12:39:30 -0400

    overhaul of setup.py.  now use `python build.py` for cythonizing, and `python setup.py nosetests` for testing

--- setup.py
+++ setup.py
@@ -4,59 +4,35 @@ ez_setup.use_setuptools()
 import glob
 import os
 import sys
-
-try:
-    from Cython.Distutils import build_ext
-
-    # Work around setuptools bug by providing a fake Pyrex, as is done here:
-    #    http://codespeak.net/svn/lxml/trunk/
-    #
-    # This seems to solve errors where the compiler is looking for .c files
-    # (which don't exist for these C++ wrappers!)
-    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "fake_pyrex"))
-
-except ImportError:
-    sys.stderr.write('''
--------------------------------------------------------------------------------
-Please install Cython 0.14.1 or greater (http://docs.cython.org) before running
-setup.py. You can use:
-
-    $ easy_install cython
-
--------------------------------------------------------------------------------
-''')
-    sys.exit(1)
-
-# Strangely, this needs to come AFTER the Cython import
 from setuptools import setup
-from setuptools.extension import Extension
+from distutils.extension import Extension
+
+if 'setuptools.extension' in sys.modules:
+    m = sys.modules['setuptools.extension']
+    m.Extension.__dict__ = m._Extension.__dict__
 
 version_py = os.path.join(os.path.dirname(__file__), 'pybedtools', 'version.py')
 version = open(version_py).read().strip().split('=')[-1].replace('"','')
-
+sources=["src/bedFile.cpp",
+         "src/fileType.cpp",
+         "src/gzstream.cpp",
+         "pybedtools/cbedtools.cpp"]
 exts = [ Extension("pybedtools.cbedtools",
-                   sources=["pybedtools/cbedtools.pyx",
-                            "pybedtools/cbedtools.pxi",
-                            "pybedtools/cbedtools.pxd"] \
-                            + glob.glob("src/*.cpp"),
+                   sources=sources,
                    libraries=["stdc++", 'z'],
+                   library_dirs=["src/"],
                    include_dirs=["src/"],
-                   depends = glob.glob("src/*.h"),
                    language="c++"),
 
          Extension('pybedtools.featurefuncs',
-                   sources=["pybedtools/featurefuncs.pyx",
-                            "pybedtools/cbedtools.pyx",
-                            "pybedtools/cbedtools.pxi",
-                            "pybedtools/cbedtools.pxd"] \
-                            + glob.glob("src/*.cpp"),
+                   sources=sources + ["pybedtools/featurefuncs.cpp"],
                    libraries=["stdc++", 'z'],
                    include_dirs=["src/"],
-                   depends = glob.glob("src/*.h"),
+                   library_dirs=["src/"],
                    language="c++"),
 
          Extension('pybedtools._Window',
-                    sources=['pybedtools/_Window.pyx'],),
+                    sources=['pybedtools/_Window.c'],),
         ]
 
 long_description = """
@@ -76,13 +52,15 @@ and see full documentation and tutorial at:
     http://packages.python.org/pybedtools
 
 """
-tests_require = ['nose']
+
+tests_require = ['nose>=0.11', 'pyyaml']
 setup(
         name="pybedtools",
         version=version,
         ext_modules=exts,
-        install_requires=['argparse','cython'],
+        install_requires=['argparse'],
         tests_require=tests_require,
+        setup_requires=tests_require,
         extras_require={'test': tests_require},
         packages=['pybedtools',
                   'pybedtools.test',
@@ -97,14 +75,13 @@ setup(
                                       "*.pxi",
                                       "*.pxd",
                                       "*.cpp"]
-                        },
+                       },
         package_dir = {"pybedtools": "pybedtools"},
         scripts = ['pybedtools/scripts/venn_gchart.py',
                    'pybedtools/scripts/venn_mpl.py',
                    'pybedtools/scripts/annotate.py',
                    'pybedtools/scripts/intron_exon_reads.py',
                    'pybedtools/scripts/pybedtools_demo.py'],
-        cmdclass = {'build_ext': build_ext},
         author_email="dalerr@niddk.nih.gov",
         classifiers=[
             'Development Status :: 4 - Beta',
--- test.sh
+++
@@ -1,3 +0,0 @@
-VERSION=$1
-python${VERSION} setup.py build_ext -i && \
-PYTHONPATH=$PYTHONPATH:. PATH=$PATH:../bedtools/bin/ nosetests-${VERSION} --with-doctest --doctest-extension=.pyx pybedtools/cbedtools.pyx .
