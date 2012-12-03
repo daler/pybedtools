d3048a38de499f416142e7e7813b3f1c89c3dc62
daler <dalerr@niddk.nih.gov>
2012-12-03 12:34:22 -0500

    improved test script

--- pybedtools/test/t_len_leak.py
+++ pybedtools/test/t_len_leak.py
@@ -1,25 +1,46 @@
 import pybedtools
+
 fn = pybedtools.example_filename('a.bed')
-orig_fds = pybedtools.helpers.n_open_fds()
 
-n1 = []
-for i in range(10):
-    x = pybedtools.BedTool(fn)
-    n1.append(pybedtools.helpers.n_open_fds())
-print max(n1) - orig_fds
-# 2
+def show_open_fds(func):
+    doc = func.__doc__
+    print
+    print doc
+    print "." * len(doc)
+    orig_fds = pybedtools.helpers.n_open_fds()
+    print '\t', max(func(fn)) - orig_fds, 'file-based'
 
-n2 = []
-for i in range(10):
-    x = pybedtools.BedTool(fn)
-    len(x)
-    n2.append(pybedtools.helpers.n_open_fds())
-print max(n2) - orig_fds
-# 10
 
-n3 = []
-for i in range(10):
-    x = pybedtools.BedTool(fn)
-    sum(1 for _ in x)
-    n3.append(pybedtools.helpers.n_open_fds())
-print max(n3) - orig_fds
+def func1(src):
+    "create bedtool in loop"
+    for i in range(10):
+        x = pybedtools.BedTool(src)
+        yield pybedtools.helpers.n_open_fds()
+
+
+def func2(src):
+    'create bedtool in loop and check length'
+    for i in range(10):
+        x = pybedtools.BedTool(src)
+        len(x)
+        yield pybedtools.helpers.n_open_fds()
+
+
+def func3(src):
+    'create bedtool outside of loop; check length inside'
+    x = pybedtools.BedTool(src)
+    for i in range(10):
+        len(x)
+        yield pybedtools.helpers.n_open_fds()
+
+
+def func4(src):
+    'create and len in loop; don\'t assign to var'
+    for i in range(10):
+        len(pybedtools.BedTool(src))
+        yield pybedtools.helpers.n_open_fds()
+
+if __name__ == "__main__":
+    for k, v in sorted(locals().items()):
+        if k.startswith('func'):
+            show_open_fds(v)
