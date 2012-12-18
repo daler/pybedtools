824a7f2ff49c746e06cdbc9ef2bfa5655a5b3912
daler <dalerr@niddk.nih.gov>
2012-12-18 15:49:38 -0500

    bigbed uses the latest bedToBigBed syntax ("-type=" instead of "-bedFields")

--- pybedtools/contrib/bigbed.py
+++ pybedtools/contrib/bigbed.py
@@ -3,14 +3,29 @@ import subprocess
 import pybedtools
 
 
-def bigbed(x, genome, output, blockSize=256, itemsPerSlot=512, bedFields=None, _as=None, unc=False):
+def bigbed(x, genome, output, blockSize=256, itemsPerSlot=512, bedtype=None, _as=None, unc=False, tab=False):
+    """
+    Converts a BedTool object to a bigBed format and returns the new filename.
+
+    `x` is a BedTool object
+
+    `genome` is an assembly string
+
+    `output` is the name of the bigBed file to create.
+
+    Other args are passed to bedToBigBed.  In particular, `bedtype` (which
+    becomes the "-type=" argument) is automatically handled for you if it is
+    kept as the default None.
+
+    Assumes that a recent version of bedToBigBed from UCSC is on the path.
+    """
     if isinstance(x, basestring):
         x = pybedtools.BedTool(x)
     if not isinstance(x.fn, basestring):
         x = x.saveas()
     chromsizes = pybedtools.chromsizes_to_file(pybedtools.chromsizes(genome))
-    if bedFields is None:
-        bedFields = x.field_count()
+    if bedtype is None:
+        bedtype = x.field_count()
     cmds = [
         'bedToBigBed',
         x.fn,
@@ -18,11 +33,15 @@ def bigbed(x, genome, output, blockSize=256, itemsPerSlot=512, bedFields=None, _
         output,
         '-blockSize=%s' % blockSize,
         '-itemsPerSlot=%s' % itemsPerSlot,
-        '-bedFields=%s' % bedFields
+        '-type=bed%s' % bedtype
     ]
     if unc:
         cmds.append('-unc')
+    if tab:
+        cmds.append('-tab')
+    p = subprocess.Popen(cmds)
+    stdout, stderr = p.communicate()
+    if p.returncode:
+        raise ValueError
 
-    os.system(' '.join(cmds))
     return output
-
