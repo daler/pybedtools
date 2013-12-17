fb62a775dd36b914c1549a192be7047332cf42df
daler <dalerr@niddk.nih.gov>
2013-12-17 11:14:37 -0500

    more travis-ci changes

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+set -e
+
+echo $PATH
+
 VERSION=$1
-python${VERSION} setup.py nosetests
+
+python${VERSION} setup.py nosetests -v
 
 # clean up from venn diagram script tests
 if [ -f out.png ]
