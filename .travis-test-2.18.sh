a16e45f2ecbcdfb3eea4de897aaa0d637bf375b7
daler <dalerr@niddk.nih.gov>
2014-01-03 18:02:36 -0500

    prepend to path in travis-ci config; better for local testing

--- .travis-test-2.18.sh
+++ .travis-test-2.18.sh
@@ -7,9 +7,9 @@ SAMTOOLS_VERSION=0.1.19
 ./.install-tabix.sh $TABIX_VERSION ${TRAVIS_BUILD_DIR}
 ./.install-bedtools2.sh ${TRAVIS_BUILD_DIR}
 ./.install-samtools.sh $SAMTOOLS_VERSION ${TRAVIS_BUILD_DIR}
-export PATH=$PATH:${TRAVIS_BUILD_DIR}/tabix-${TABIX_VERSION}
-export PATH=$PATH:${TRAVIS_BUILD_DIR}/samtools-${SAMTOOLS_VERSION}
-export PATH=$PATH:${TRAVIS_BUILD_DIR}/bedtools2/bin
+export PATH=${TRAVIS_BUILD_DIR}/tabix-${TABIX_VERSION}:$PATH
+export PATH=${TRAVIS_BUILD_DIR}/samtools-${SAMTOOLS_VERSION}:$PATH
+export PATH=${TRAVIS_BUILD_DIR}/bedtools2/bin:$PATH
 echo $PATH
 nosetests -v
 cd docs && make clean && make doctest
