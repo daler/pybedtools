1eeb266ac9289258d6541ced37318bec62951efd
daler <dalerr@niddk.nih.gov>
2014-05-23 11:43:38 -0400

    travis config for BEDTools v2.20.0

--- .install-bedtools2.sh
+++ .install-bedtools2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR=$1
+BRANCH=$2
 git clone https://github.com/arq5x/bedtools2.git ${DIR}/bedtools2
 cd ${DIR}/bedtools2
-git checkout v2.19.1
+git checkout $BRANCH
 make
--- .travis-test.sh
+++ .travis-test.sh
@@ -4,12 +4,13 @@ set -e
 
 TABIX_VERSION=0.2.6
 SAMTOOLS_VERSION=0.1.19
+BEDTOOLS_BRANCH=v2.20.0
 ./.install-tabix.sh $TABIX_VERSION ${TRAVIS_BUILD_DIR}
-./.install-bedtools.sh ${TRAVIS_BUILD_DIR}
+./.install-bedtools2.sh ${TRAVIS_BUILD_DIR} ${BEDTOOLS_BRANCH}
 ./.install-samtools.sh $SAMTOOLS_VERSION ${TRAVIS_BUILD_DIR}
-export PATH=$PATH:${TRAVIS_BUILD_DIR}/tabix-${TABIX_VERSION}
-export PATH=$PATH:${TRAVIS_BUILD_DIR}/samtools-${SAMTOOLS_VERSION}
-export PATH=$PATH:${TRAVIS_BUILD_DIR}/bedtools/bin
+export PATH=${TRAVIS_BUILD_DIR}/tabix-${TABIX_VERSION}:$PATH
+export PATH=${TRAVIS_BUILD_DIR}/samtools-${SAMTOOLS_VERSION}:$PATH
+export PATH=${TRAVIS_BUILD_DIR}/bedtools2/bin:$PATH
 echo $PATH
 nosetests -v
 cd docs && make clean && make doctest
