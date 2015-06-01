fde94963c3dbe4024886b774dc5f0d03a72571ad
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-06-01 17:11:07 +0100

    made gzip tests test less in one test

--- pybedtools/test/test_gzip_support.py
+++ pybedtools/test/test_gzip_support.py
@@ -2,10 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-
+from nose.tools import assert_equal
 import pybedtools.test.tfuncs as tfuncs
 
-import os
 import pybedtools
 import gzip
 
@@ -13,28 +12,43 @@ setup = tfuncs.setup
 teardown = tfuncs.teardown
 
 def _make_temporary_gzip(bed_filename):
+    """
+    Make a gzip file on the fly
+    :param bed_filename: Filename of bed file to gzip
+    :return: filename of gzipped file
+    """
     gz_filename = pybedtools.BedTool._tmp()
     with gzip.open(gz_filename, 'w') as out_:
         with open(bed_filename, 'r') as in_:
             out_.writelines(in_)
     return gz_filename
 
-def test_gzip():
-    # make new gzipped files on the fly
+def test_gzipped_file_types_are_bed():
+    agz = _make_temporary_gzip(pybedtools.example_filename('a.bed'))
+
+    agz = pybedtools.BedTool(agz)
+    assert_equal('bed', agz.file_type)
+
+def test_gzipped_files_can_be_intersected():
     agz = _make_temporary_gzip(pybedtools.example_filename('a.bed'))
     bgz = _make_temporary_gzip(pybedtools.example_filename('b.bed'))
 
     agz = pybedtools.BedTool(agz)
     bgz = pybedtools.BedTool(bgz)
-    assert agz.file_type == bgz.file_type == 'bed'
+
     a = pybedtools.example_bedtool('a.bed')
     b = pybedtools.example_bedtool('b.bed')
     assert a.intersect(b) == agz.intersect(bgz) == a.intersect(bgz) == agz.intersect(b)
 
 
 def test_gzipped_output():
-    expected = pybedtools.BedTool._tmp()
-    fn = pybedtools.example_filename('a.bed')
-    os.system('gzip -c {fn} > {expected}'.format(**locals()))
-    obs = pybedtools.example_bedtool('a.bed').saveas(compressed=True)
-    assert gzip.open(obs.fn).read() == gzip.open(expected).read()
+    _filename = pybedtools.example_filename('a.bed')
+    compressed_file = pybedtools.BedTool(_filename).saveas(compressed=True)
+
+    with gzip.open(compressed_file.fn) as gf:
+        uncompressed_content = gf.read()
+
+    with open(_filename) as f:
+        original_content = f.read()
+
+    assert_equal(original_content, uncompressed_content)
