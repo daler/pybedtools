f97b5f46b7cc4e779a2f4fa0e22136d0f058f89c
daler <dalerr@niddk.nih.gov>
2015-06-10 10:29:29 -0400

    udpate .travis-test.sh

--- .travis-test.sh
+++ .travis-test.sh
@@ -4,14 +4,19 @@ set -e
 
 TABIX_VERSION=0.2.6
 SAMTOOLS_VERSION=0.1.19
-BEDTOOLS_BRANCH=v2.22.1
+BEDTOOLS_BRANCH=v2.24.0
 ./.install-tabix.sh $TABIX_VERSION ${TRAVIS_BUILD_DIR}
 ./.install-bedtools2.sh ${TRAVIS_BUILD_DIR} ${BEDTOOLS_BRANCH}
 ./.install-samtools.sh $SAMTOOLS_VERSION ${TRAVIS_BUILD_DIR}
+
 export PATH=${TRAVIS_BUILD_DIR}/tabix-${TABIX_VERSION}:$PATH
 export PATH=${TRAVIS_BUILD_DIR}/samtools-${SAMTOOLS_VERSION}:$PATH
 export PATH=${TRAVIS_BUILD_DIR}/bedtools2/bin:$PATH
+
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+source activate test-environment
+
 echo $PATH
 nosetests -v
 cd docs && make clean && make doctest
-
