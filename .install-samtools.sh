7072d8ad86fa0590b5220e8d94ac4541ec2f4f29
daler <dalerr@niddk.nih.gov>
2015-08-02 15:01:56 -0400

    clean up old test infrastructure

--- .install-samtools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=$1
-DIR=$2
-cd ${DIR}
-wget http://sourceforge.net/projects/samtools/files/samtools/${VERSION}/samtools-${VERSION}.tar.bz2
-tar -xjvf samtools-${VERSION}.tar.bz2
-cd samtools-${VERSION}
-make
--- .install-tabix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=$1
-DIR=$2
-cd ${DIR}
-wget http://sourceforge.net/projects/samtools/files/tabix/tabix-${VERSION}.tar.bz2
-tar -xjvf tabix-${VERSION}.tar.bz2
-cd tabix-${VERSION}
-make
