b6ec730f31005c143318d1a49af72a52de98b6ca
daler <dalerr@niddk.nih.gov>
2016-04-15 21:52:40 -0400

    fix test for py3

--- pybedtools/test/test1.py
+++ pybedtools/test/test1.py
@@ -2057,5 +2057,5 @@ def test_issue_169():
     x = pybedtools.example_bedtool("1000genomes-example.vcf")
     fn = x.bgzip(is_sorted=False, force=True)
     line = gzip.open(fn).readline()
-    assert line.startswith('#'), line
+    assert str(line).startswith('#'), line
 
