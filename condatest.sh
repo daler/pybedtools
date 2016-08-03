52a6005abe4363cf2ab3710d759c2d1523644e42
daler <dalerr@niddk.nih.gov>
2016-08-03 10:08:53 -0400

    add nose to requirements; deactivate env before deleting

--- condatest.sh
+++ condatest.sh
@@ -13,40 +13,51 @@ PY_VERSION=$1
 usage="Usage: $0 py_version[2|3]"
 : ${PY_VERSION:?$usage}
 
-# Ensure we're starting the environment from scratch
+log () {
+    echo
+    echo "[`date`] TEST HARNESS: $1"
+    echo
+}
+
+log "removing existing env pbtpy${PY_VERSION}"
 name=pbtpy${PY_VERSION}
 conda env remove -y -n $name
 
-# Force the re-Cythonizing
-rm -rf dist build
+log "starting with basic environment"
+conda create -y -n $name --channel bioconda python=${PY_VERSION} bedtools=2.25.0
+source activate $name
 
+log "temporarily install cython"
+conda install cython
+
+log "force re-cythonizing"
+rm -rf dist build
 python setup.py clean
 python setup.py build
 python setup.py sdist
 
-conda create \
-    -y \
-    -c daler \
-    -n $name \
-    python=${PY_VERSION} \
-    bedtools=2.25.0 \
-    matplotlib \
-    sphinx \
-    numpydoc \
-    tabix \
-    pysam \
-    nose \
-    six \
-    pyyaml \
-    pandas
+log "uninstall cython"
+conda remove cython
 
+log "test installation of sdist"
+set -x
+(cd dist && pip install pybedtools-*.tar.gz && python -c 'import pybedtools')
+set +x
+
+python setup.py clean
+
+log "install test requirements"
+source deactivate
+conda env remove -y -n $name
+conda create -y -n $name --channel bioconda python=${PY_VERSION} \
+    --file "requirements.txt" \
+    --file "test-requirements.txt" \
+    --file "optional-requirements.txt"
 source activate $name
 
-# test installation via pip; just test that we can import successfully:
-pip install dist/pybedtools-*.tar.gz
-(cd docs && python -c 'import pybedtools')
-
-# Now actually build from source dir
+log "install pybedtools from setup.py in develop mode to trigger re-cythonizing"
 python setup.py develop
+
+log "run tests"
 nosetests
 (cd docs && make clean && make doctest)
