abc8ee557a57ea63755dd13b589608290ed3e73c
Ryan Dale <ryan.dale@nih.gov>
2018-11-19 15:57:54 -0500

    rm classifier, which has been unsupported for a while

--- pybedtools/contrib/__init__.py
+++ pybedtools/contrib/__init__.py
@@ -2,5 +2,4 @@ from . import bigwig
 from . import bigbed
 from . import venn_maker
 from . import long_range_interaction
-from .classifier import MultiClassifier
 from .intersection_matrix import IntersectionMatrix
