4d95ef60ad4176f60b2e54f15da7c9b4498e344b
Ryan Dale <ryan.dale@nih.gov>
2018-11-19 15:55:28 -0500

    support fetchChromSizes in addition to mysql

--- pybedtools/helpers.py
+++ pybedtools/helpers.py
@@ -618,17 +618,25 @@ def internet_on(timeout=1):
     return False
 
 
-def get_chromsizes_from_ucsc(genome, saveas=None, mysql='mysql', timeout=None):
+def get_chromsizes_from_ucsc(genome, saveas=None, mysql='mysql',
+                             fetchchromsizes='fetchChromSizes', timeout=None):
     """
     Download chrom size info for *genome* from UCSC and returns the dictionary.
 
-    If you need the file, then specify a filename with *saveas* (the dictionary
-    will still be returned as well).
+    Parameters
+    ----------
 
-    If ``mysql`` is not on your path, specify where to find it with
-    *mysql=<path to mysql executable>*.
+    genome : str
+        Name of the genome assembly (e.g., "hg38")
 
-    *timeout* is how long to wait for a response; mostly used for testing.
+    saveas : str
+        Filename to save output to. Dictionary will still be returned.
+
+    mysql, fetchchromsizes : str
+        Paths to MySQL and fetchChromSizes.
+
+    timeout : float
+        How long to wait for a response; mostly used for testing.
 
     Example usage:
 
@@ -661,6 +669,8 @@ def get_chromsizes_from_ucsc(genome, saveas=None, mysql='mysql', timeout=None):
             '-A',
             '-e',
             'select chrom, size from %s.chromInfo' % genome]
+    failures = []
+    d = {}
     try:
         p = subprocess.Popen(cmds,
                              stdout=subprocess.PIPE,
@@ -673,7 +683,6 @@ def get_chromsizes_from_ucsc(genome, saveas=None, mysql='mysql', timeout=None):
             print((subprocess.list2cmdline(cmds)))
 
         lines = stdout.splitlines()[1:]
-        d = {}
         for line in lines:
             if isinstance(line, bytes):
                 line = line.decode('UTF-8')
@@ -683,16 +692,42 @@ def get_chromsizes_from_ucsc(genome, saveas=None, mysql='mysql', timeout=None):
         if saveas is not None:
             chromsizes_to_file(d, saveas)
 
-        return d
 
     except OSError as err:
         if err.errno == 2:
-            raise OSError("Can't find mysql -- if you don't have it "
-                          "installed, you'll have to get chromsizes "
-                          " manually, or "
-                          "specify the path with the 'mysql' kwarg.")
+            failures.append("Can't find mysql at path {0}".format(mysql))
         else:
             raise
+    try:
+        cmds = [fetchchromsizes, genome]
+        p = subprocess.Popen(cmds,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             bufsize=1)
+        stdout, stderr = p.communicate()
+        if stderr:
+            if 'INFO: trying WGET' not in str(stderr):
+                print(stderr)
+                print('Commands were:\n')
+                print((subprocess.list2cmdline(cmds)))
+
+        lines = stdout.splitlines()
+        for line in lines:
+            if isinstance(line, bytes):
+                line = line.decode('UTF-8')
+            chrom, size = line.split()
+            d[chrom] = (0, int(size))
+
+        if saveas is not None:
+            chromsizes_to_file(d, saveas)
+
+    except OSError as err:
+        if err.errno == 2:
+            failures.append("Can't find path to fetchChromsizes")
+
+    if not d:
+        raise OSError(failures)
+    return d
 
 
 def chromsizes_to_file(chrom_sizes, fn=None):
